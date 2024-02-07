2c89aad4422e76cde6de858296cee4dd382ac3e3
lonewolf <bajpaiutkarsh1104@gmail.com>
2024-02-07 13:36:35 +0530

    project init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('home')
+} )
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port : http://localhost:8080');
+})
